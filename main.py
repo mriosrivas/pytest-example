bb0f16f08040063b8584436be793d5b51569e242
Manuel <mriosrivas@gmail.com>
2022-12-06 16:08:39 -0600

    Add initial files

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def calc_distance(x1, x2):
+    return np.sqrt(np.sum(np.square(x2 - x1)))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(calc_distance(5,10))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
